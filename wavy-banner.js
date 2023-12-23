2b90a4f658a8e0e7560d9452be92ecd070adb237
Tyler Mumford <tyler.mumford@icloud.com>
2023-12-23 01:52:43 -0700

    Experimenting with wavy banners

--- wavy-banner.js
+++ wavy-banner.js
@@ -1,3 +1,10 @@
+// Copyright (c) 2023 by Shaw (https://codepen.io/shshaw/pen/JbrQrW)
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Modified by tylermumford
+
 console.clear();
 
 let mesh;
@@ -7,7 +14,7 @@ let spacingY = 5;
 let accuracy = 1;
 
 let opts = {
-  image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/39255/face.png',
+  image: 'http://localhost:8080/banner-single-ravenclaw.png',
   gravity: 200,
   friction: 0.99,
   bounce: 0.3,
